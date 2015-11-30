a73139de73a7bd00207fd494c6153c81b23a12c1
jimmy <sunyongjie0404@gmail.com>
2015-11-30 09:37:17 +0800

    rename file name

---
+++ agent/agentconfig.cpp
@@ -0,0 +1,12 @@
+/**
+ * @authors Jimmy Sun (yongjie.sun@intel.com)
+ * @date    2015-11-30 09:27:26
+ * @version 1.0
+ */
+
+#include "agentconfig.hpp"
+
+
+AgentConfig::AgentConfig(string configFilePath) {
+
+}
