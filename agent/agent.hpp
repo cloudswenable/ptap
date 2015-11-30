9a69ac29a718f8516279a0cc2d00e34f5a265903
jimmy <sunyongjie0404@gmail.com>
2015-11-30 09:36:08 +0800

    rename ServerConfig to AgentConfig

--- agent/agent.hpp
+++ agent/agent.hpp
@@ -6,4 +6,20 @@
 #ifndef AGENT_H_
 #define AGENT_H_
 
+#include "agentconfig.hpp"
+
+class Agent {
+public:
+	Agent();
+	Agent(string configFilePath);
+
+private:
+	/** command port for upd connection*/
+	const int _cmd_port;
+	/** data transfer port for tcp connection */
+	int _data_port;
+	/** server configuration */
+	AgentConfig _config;
+};
+
 #endif // AGENT_H
--- agent/serverconfig.hpp
+++ agent/serverconfig.hpp
@@ -3,16 +3,16 @@
  * @date    2015-11-30 09:16:10
  * @version 1.0
  */
-#ifndef SERVER_CONFIG_H 
-#define SERVER_CONFIG_H
+#ifndef AGENT_CONFIG_H 
+#define AGENT_CONFIG_H
 
-class ServerConfig {
+class AgentConfig {
 public:
-	ServerConfig(string configFilePath = "server.cfg");
+	AgentConfig(string configFilePath = "server.cfg");
 private:
 	const string _server_ip;
 	const int _server_port;
 	const int _heatbeat_interval;
 };
 
-#endif //SERVER_CONFIG_H
+#endif //AGENT_CONFIG_H
